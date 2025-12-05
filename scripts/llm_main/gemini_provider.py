e6f3faf891d1a63f3e1bb88d048097f6e96a49f8
hugo <hugo.prd.br@gmail.com>
2025-12-05 19:52:46 -0300

    Volta pro gemini flash

--- scripts/llm_main/gemini_provider.py
+++ scripts/llm_main/gemini_provider.py
@@ -13,14 +13,8 @@ def get_gemini_llm(temperature: float = 0.3):
     print("--- Inicializando Conexão com Google Gemini ---")
 
     return ChatGoogleGenerativeAI(
-        model="gemini-pro",  # "gemini-1.5-flash",
+        model="gemini-1.5-flash",
         google_api_key=api_key,
         temperature=temperature,
         convert_system_message_to_human=True,
-        safety_settings={
-            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-        },
     )
